0a146a82c1a41c8491c442b028abe35ec4339d34
Ryeem Zia <ryeemzia@gmail.com>
2021-09-26 13:32:49 -0400

    Revert "Merge pull request #2 from camilohv/main"
    This reverts commit ed1f2a2356fe0c6d2e60f0a14fbe4a8ea6972ff2, reversing
    changes made to 44266792a11849912d1e8fb92fdaab175942d926.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
-import LogData from "./components/log-data.component";
+import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <br/>
         <Route path="/" exact component={ExercisesList} />
         <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={LogData} />
+        <Route path="/create" component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
       </div>
     </Router>
